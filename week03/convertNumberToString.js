35e954bbb5960aa0079595c34bddb5d62ee3c51b
moling <365024424@qq.com>
2020-04-25 00:04:21 +0800

    Create convertNumberToString.js

---
+++ week03/convertNumberToString.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number} number
+ * @param {number} x
+ * @return {string}
+ */
+function convertNumberToString(number, x = 10) {
+  let integer = Math.floor(number);
+  let decimal = number - integer;
+  let string = !integer ? '0' : '';
+  while (integer > 0) {
+    string = `${integer % x}${string}`;
+    integer = Math.floor(integer / x);
+  }
+
+  if (decimal) {
+    string += '.';
+    while (decimal && !/\.\d{20}$/.test(string)) { // 最大保留20位小数
+      decimal *= x;
+      string += `${Math.floor(decimal)}`;
+      decimal -= Math.floor(decimal);
+    }
+  }
+  return string;
+}
+
+console.log(convertNumberToString(123.456, 10)); // 123.456000000000
+console.log(convertNumberToString(10.25, 8)); // 12.2
+console.log(convertNumberToString(16.5, 16)); // 10.8
