2048b9299e3c568841755ee6692ff8edd12b3636
moling3650 <365024424@qq.com>
2020-06-02 23:52:58 +0800

    2.2.1 属性选择匹配器添加支持属性值的简单匹配

--- week08/match.js
+++ week08/match.js
@@ -11,14 +11,24 @@ function matchByIdSelector(selector, element) {
 }
 
 function matchByAttributeSelector(selector, element) {
-  const match = /^\[\s*([\w-]+).*\]$/.exec(selector)
+  const match = /^\[\s*([\w-]+)\s*(?:(=)\s*(\S+))?\s*\]$/.exec(selector)
   if (!match) {
     return false
   }
   // 属性名比较
   const name = match[1]
   const attrValue = element.getAttribute(name)
-  return attrValue === null
+  if (attrValue === null) {
+    return false
+  }
+  // 没有比较符号就没有属性值的比较
+  const comparator = match[2]
+  if (!comparator) {
+    return true
+  }
+  // 属性值比较
+  const value = match[3].replace(/["']/g, '') // 去除value的引号
+  return attrValue === value
 }
 
 // 检查一个元素和简单选择器是否匹配
