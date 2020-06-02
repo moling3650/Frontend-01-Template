d612d29dda7bb042ca06290b828af17900f67de5
moling3650 <365024424@qq.com>
2020-06-02 23:12:37 +0800

    2.1 添加ID选择匹配器

--- week08/match.js
+++ week08/match.js
@@ -6,10 +6,16 @@ function matchByTypeSelector(selector, element) {
   return element.tagName === selector.toUpperCase()
 }
 
+function matchByIdSelector(selector, element) {
+  return element.id === selector.replace('#', '')
+}
+
 // 检查一个元素和简单选择器是否匹配
 function matchBySimpleSelector(selector, element) {
   if (!selector || !element) {
     return false
+  } else if (selector.startsWith('#')) { // HASH
+    return matchByIdSelector(selector, element)
   } else if (selector.startsWith('.')) { // class
     return matchByClassSelector(selector, element)
   } else { // type_selector
