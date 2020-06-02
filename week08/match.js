406067413b4dbc4aaf60de9e59a27483f0d7f3cc
moling3650 <365024424@qq.com>
2020-06-02 23:05:25 +0800

    2.0 添加一个标签选择匹配器，并添加一个简单选择匹配器作为上层调用

--- week08/match.js
+++ week08/match.js
@@ -1,3 +1,18 @@
 function matchByClassSelector(selector, element) {
   return element.className.split(/\s+/g).includes(selector.replace('.', ''))
 }
+
+function matchByTypeSelector(selector, element) {
+  return element.tagName === selector.toUpperCase()
+}
+
+// 检查一个元素和简单选择器是否匹配
+function matchBySimpleSelector(selector, element) {
+  if (!selector || !element) {
+    return false
+  } else if (selector.startsWith('.')) { // class
+    return matchByClassSelector(selector, element)
+  } else { // type_selector
+    return matchByTypeSelector(selector, element)
+  }
+}
